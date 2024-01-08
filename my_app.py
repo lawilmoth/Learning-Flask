7afdcfaf8047570d1a4a3115f6278547d04923fa
mr_wilmoth <lwilmoth@eriesd.org>
2024-01-08 12:12:36 -0500

    initial

--- my_app.py
+++ my_app.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
 from flask import request
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "hard_to_guess_string"
+
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+
 
 @app.route('/')
 def index():    
@@ -13,6 +23,14 @@ def index():
 def user(name):
 
     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def intenal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
