d7b9a035501e91022244ea464ea5b43b28f7c686
mr_wilmoth <lwilmoth@eriesd.org>
2024-01-29 15:17:47 -0500

    Login form is up, but there is an error querying

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
+from flask_login import UserMixin
+from . import db, login_manager
 
 
 class Role(db.Model):
@@ -14,9 +15,10 @@ class Role(db.Model):
     
 
     
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique = True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
@@ -35,3 +37,8 @@ class User(db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
