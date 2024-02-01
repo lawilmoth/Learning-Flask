a3bc6640364112dde428707966abb3970578e483
mr_wilmoth <lwilmoth@eriesd.org>
2024-02-01 14:24:28 -0500

    not done with registration

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1,64),
@@ -8,3 +10,11 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[
+        DataRequired(), Length(1,64), Email()])
+    username = StringField('Username', validators=[
+        DataRequired(), Length(1,64),
+        Regexp['^[A-Za-z][A-Za-z0-9_.]*$',0,
+               'Usernames must only contain letters, numbers, dots, or underscores']])
